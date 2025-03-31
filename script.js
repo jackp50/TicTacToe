028b692d385be85c74b593d215e0feb03a5d4b21
jackp50 <jpearne9@gmail.com>
2025-03-30 21:30:32 -0600

    Added a win condition function

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const xClass = "x";
 const circleClass = "circle";
-
+const winningCombinations = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+]
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board');
 let circleTurn;
@@ -21,6 +25,9 @@ function handleClick(e) {
     const currentClass = circleTurn ? circleClass : xClass;
     //placeMark
     placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        console.log("winner")
+    }
     //Check for win
     //check for Draw
     //switch turns
@@ -46,4 +53,12 @@ function setBoardHoverClass() {
         board.classList.add(xClass)
     }
 
+}
+
+function checkWin(currentClass) {
+   return winningCombinations.some(combinations => {
+    return combinations.every(index => {
+        return cellElements[index].classList.contains(currentClass);
+    })
+   })
 }
